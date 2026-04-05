80f3019d37a4450a33a9c1e8b06f9c3911c3a2d5
Pranika Sharan <pranikasharan14@gmail.com>
2026-04-05 19:24:02 +0530

    Implement TOTP OTP generation and countdown
    Add TOTP OTP generator with countdown display

--- countdown.py
+++ countdown.py
@@ -1,3 +1,12 @@
 import pyotp
 import time
 
+user_key = "ABWYX7SJARIP7SNS"
+otp_generator = pyotp.TOTP(user_key)
+
+while TRUE:
+current_otp = otp_generator.now()
+rem_Secs = 30 - int(time.time()) % 30
+
+printf(f"Code: {current_otp} | Expires in: {rem_Secs}s", end = "\r")
+time.off(1)
